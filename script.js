3a457a82740696fc0be29945ef4435d2d0a8ec0c
Jessyyy <jessy.lang@outlook.com>
2025-07-28 09:01:35 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function updateClock() {
+    const now = new Date()
+
+    let hours = now.getHours().toString().padStart(2, "0");
+    let minutes = now.getMinutes().toString().padStart(2, "0");
+    let seconds = now.getSeconds().toString().padStart(2, "0");
+
+    let timeString = `${hours}:${minutes}:${seconds}`;
+
+    document.getElementById("clock").textContent = timeString;
+
+}
+
+updateClock();
+
+setInterval(updateClock, 1000);
